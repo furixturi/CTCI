bb9accf9c223ff8c3043f2b29a96ad15c3f75a6f
furixturi <furixturi@gmail.com>
2020-03-15 22:34:12 +0900

    Add 10.3

---
+++ 10 sorting and searching/10.3 searchInRotatedArray.py
@@ -0,0 +1,46 @@
+# Search in Rotated Array: Given a sorted array of n integers that has
+# been rotated an unknown number of times, write code to find an element
+# in the array. You may assume that the array was originally sorted in
+# increasing order.
+# EXAMPLE
+# Input:find 5 in {15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14} 
+# Output: 8 (the index of 5 in the array)
+
+def searchRotatedArray(nums, t):
+  l = 0
+  r = len(nums) - 1
+  p = -1
+  if nums[l] < nums[r]:
+    return bs(nums, t)
+  while l < r:
+    m = (l + r) // 2
+    if nums[m] < nums[r]:
+      if nums[m-1] > nums[m]:
+        p = m
+        break
+      r = m-1
+    else:
+      if nums[m+1] < nums[m]:
+        p = m+1
+        break
+      l = m+1
+  rLeft = bs(nums, t, 0, p-1)
+  if rLeft != -1:
+    return rLeft
+  rRight = bs(nums, t, p, r)
+  return rRight
+
+def bs(nums, t, l=0, r=None):
+  r = len(nums) - 1 if r is None else r
+  if l <= r:
+    m = (l + r) // 2
+    if t == nums[m]:
+      return m
+    if t < nums[m]:
+      return bs(nums, t, l, m-1)
+    if t > nums[m]:
+      return bs(nums, t, m+1, r)
+  return -1
+
+print(searchRotatedArray([3,4,5,1,2],1))
+print(searchRotatedArray([3,4,5,1,2],6))

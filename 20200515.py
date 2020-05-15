7acf17a6c4f48ddc72649d1c0407c1b6464b1bd0
furixturi <furixturi@gmail.com>
2020-05-15 12:58:07 +0900

    20200515 improve non-existent case fallback

--- 20200515.py
+++ 20200515.py
@@ -14,41 +14,37 @@
 
 # {key1: [{value:value1, time:4}, {value:value2, time: 6}]
 
-
-
-
+from operator import attrgetter
 class TimeMap:
-	def __init__(self):
-		self.items = {}
-	def set(self, key, value, time):
-		if key in self.items:
-			self.items[key].append({value: value, time: time})
-		else:
-			self.items[key] = [{value: value, time: time}]
-	def get(self, key, time):
-		if key not in self.items or self.items[key][0].time > time:
-			return None
-		values = self.items[key]
-		start = 0
-		end = len(values) - 1
-		while start <= end:
-			mid = (start + end) // 2
-			if values[mid].time == time:
-				return values[mid].value
-			if values[mid].time < time:
-				start = mid+1
-				continue
-			else:
-				if end-start <= 1:
-					if values[end].time == time:
-						return values[end].value
-					if values[start].time == time:
-						return values[start].value
-					if values[end].time < time:
-						return values[end].value
-					else:
-						return values[start-1].value
-				end = mid-1
-		return None
-					
-				
+  def __init__(self):
+    self.items = {}
+
+  def set(self, key, value, time):
+    if key in self.items:
+      self.items[key].append({value: value, time: time})
+      self.items[key].sort(key=attrgetter('time'))
+    else:
+      self.items[key] = [{value: value, time: time}]
+
+  def get(self, key, time):
+    if key not in self.items or self.items[key][0].time > time:
+      return None
+    values = self.items[key]
+    start = 0
+    end = len(values) - 1
+    while start <= end:
+      mid = (start + end) // 2
+      if values[mid].time == time:
+        return values[mid].value
+      if values[mid].time < time:
+        if mid+1 < len(values) and values[mid+1].time > time:
+          return values[mid].value
+        start = mid+1
+      else:
+        # values[mid].time > time
+        if mid-1 >= 0 and values[mid-1].time < time:
+          return values[mid-1].value
+        end = mid-1
+    return None
+          
+        
